79dd492719f7a141218624b6ccb039984b9c7933
Rupam Sutar <107876344+rupamsutar@users.noreply.github.com>
2023-10-27 11:44:45 +0530

    Sidebar search component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SidebarHeader } from './header'
 import Notifications from './notifications/Notifications'
+import { Search } from './search'
 
 const Sidebar = () => {
+    const [searchResults, setSearchResults] = useState([]);
   return (
     <div className="w-[40%] h-full select-none">
         {/* Sidebar Header */}
         <SidebarHeader />
         {/* Notifications */}
         <Notifications />
+        {/* Search */}
+        <Search searchLength={searchResults.length} /> 
     </div>
   )
 }
